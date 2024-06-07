70e233b36a56bfa3d7eee22e26eb25e5be51faf9
fakhryhizballah <falehry88@gmail.com>
2024-06-07 11:35:58 +0700

    chore: Update JSON file names and fix code to import and update batch data

--- controlers/gpu.js
+++ controlers/gpu.js
@@ -7,3 +7,16 @@ console.log(kernel()); // [Float32Array([1,2,3,4])];
 const json = kernel.toJSON();
 const newKernelFromJson = gpu.createKernel(json);
 console.log(newKernelFromJSON()); // [Float32Array([1,2,3,4])];
+
+
+async function zz() {
+    // let x = await client.keys('data:SEP:klaim:2:*');
+    let x = await client.keys('data:monitoring:klaim:2023-06-01:2023-06-02:*');
+    console.log(x);
+    for (let i = 0; i < x.length; i++) {
+        console.log(x[i]);
+        client.del(x[i]);
+    }
+}
+
+// zz();    
