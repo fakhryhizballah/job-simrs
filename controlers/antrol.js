5dd33ccee341d3712c8f6eefd2acc6879e2fae77
fakhryhizballah <falehry88@gmail.com>
2024-08-14 07:58:58 +0700

    add rujukan internal

--- controlers/antrol.js
+++ controlers/antrol.js
@@ -27,138 +27,67 @@ async function addAntreanJKN(date) {
     if (res.metadata.code == 200) {
         // console.log(res);
         let filter = res.response.filter((item) => item.ispeserta === true);
+        filter = filter.filter((item) => item.status === true);
+        console.log(filter);
         kodebooking = filter.map((item) => item.kodebooking);
     }
+    console.log(res.response)
+    return;
     // console.log(kodebooking);
-    let regBooking = await reg_periksa.findAll({
-        where: {
-            no_rawat: { [Op.notIn]: kodebooking },
-            tgl_registrasi: date,
-            kd_pj: 'BPJ',
-            status_lanjut: 'Ralan',
-            kd_poli: { [Op.notIn]: ['IGDK', 'U0003', 'U0008', 'U0022', 'U0055', 'U0054'] },
-        },
-        include: [{
-            model: maping_poli_bpjs,
-            as: 'maping_poli_bpjs',
-            attributes: ['kd_poli_bpjs', 'nm_poli_bpjs']
-        }, {
-            model: maping_dokter_dpjpvclaim,
-            as: 'maping_dokter_dpjpvclaim',
-            attributes: ['kd_dokter_bpjs', 'nm_dokter_bpjs']
-        }, {
-            model: pasien,
-            as: 'pasien',
-            attributes: ['no_ktp', 'no_tlp', 'no_peserta']
-        }, {
-            model: bridging_sep,
-            as: 'bridging_sep',
-            attributes: ['no_rujukan', 'noskdp', 'no_sep']
-        }
-        ],
-        attributes: ['no_reg', 'no_rawat', 'tgl_registrasi', 'no_rkm_medis', 'jam_reg', 'kd_pj', 'kd_dokter', 'kd_poli', 'status_poli'],
-        order: [
-            ['jam_reg', 'DESC'],
-        ],
-    });
-    let sepNull = 0;
-    for (let element of regBooking) {
-        if (element.bridging_sep == null) {
-            sepNull++;
-            continue;
-        }
-        let noRef = element.bridging_sep.no_rujukan;
-        console.log(noRef);
-        console.log(element.maping_dokter_dpjpvclaim.kd_dokter_bpjs);
-        if (noRef.length != 19) {
-            noRef = element.bridging_sep.noskdp;
-            if (noRef.length != 19) {
-                noRef = element.bridging_sep.no_sep;
-            }
-        }
-        console.log(element.status_poli)
-        console.log(element.no_rawat)
-        console.log(element.no_rkm_medis)
-        element.maping_poli_bpjs.kd_poli_bpjs == "096" ? element.maping_poli_bpjs.kd_poli_bpjs = "PAR" : element.maping_poli_bpjs.kd_poli_bpjs
-        let jadwalDr = await client.json.get(`Antrol:${date}:${element.maping_poli_bpjs.kd_poli_bpjs}`)
-        if (jadwalDr == null) {
-            jadwalDr = await jddokter(date, element.maping_poli_bpjs.kd_poli_bpjs);
-            jadwalDr = jadwalDr.response
-            client.json.set(`Antrol:${date}:${element.maping_poli_bpjs.kd_poli_bpjs}`, '$', jadwalDr)
-            client.expire(`Antrol:${date}:${element.maping_poli_bpjs.kd_poli_bpjs}`, 3600)
-        }
-        let jadwals = jadwalDr.find((item) => item.kodedokter == element.maping_dokter_dpjpvclaim.kd_dokter_bpjs);
-        let estimasidilayani = convmils(`${element.tgl_registrasi} ${element.jam_reg}`, 30);
-        console.log(element.no_rawat);
-        let data = {
-            kodebooking: element.no_rawat,
-            jenispasien: "JKN",
-            nomorkartu: element.pasien.no_peserta,
-            nik: element.pasien.no_ktp,
-            nohp: element.pasien.no_tlp,
-            kodepoli: element.maping_poli_bpjs.kd_poli_bpjs,
-            namapoli: element.maping_poli_bpjs.nm_poli_bpjs,
-            pasienbaru: element.stts_daftar == "Baru" ? 1 : 0,
-            norm: element.no_rkm_medis,
-            tanggalperiksa: element.tgl_registrasi,
-            kodedokter: element.maping_dokter_dpjpvclaim.kd_dokter_bpjs,
-            namadokter: element.maping_dokter_dpjpvclaim.nm_dokter_bpjs,
-            jampraktek: jadwals.jadwal || "-",
-            jeniskunjungan: 3,
-            nomorreferensi: noRef,
-            nomorantrean: `${element.maping_poli_bpjs.kd_poli_bpjs}-${element.no_reg}`,
-            angkaantrean: parseInt(element.no_reg),
-            estimasidilayani: estimasidilayani,
-            sisakuotajkn: (jadwals.kapasitaspasien - parseInt(element.no_reg)),
-            kuotajkn: jadwals.kapasitaspasien,
-            sisakuotanonjkn: (jadwals.kapasitaspasien - parseInt(element.no_reg)),
-            kuotanonjkn: jadwals.kapasitaspasien,
-            keterangan: "Peserta harap 20 menit lebih awal guna pencatatan administrasi.",
-        };
-        console.log(data);
-        let random1 = getRandomInt(10, 15);
-        let waktuTask1 = convmils(`${element.tgl_registrasi} ${element.jam_reg}`, -random1);
-        let taks1 = {
-            kodebooking: element.no_rawat,
-            taskid: 1,
-            waktu: waktuTask1,
-        };
+    // let regBooking = await reg_periksa.findAll({
+    //     where: {
+    //         no_rawat: { [Op.notIn]: kodebooking },
+    //         tgl_registrasi: date,
+    //         kd_pj: 'BPJ',
+    //         status_lanjut: 'Ralan',
+    //         kd_poli: { [Op.notIn]: ['IGDK', 'U0003', 'U0008', 'U0022', 'U0055', 'U0054'] },
+    //     },
+    //     attributes: ['no_reg', 'no_rawat', 'tgl_registrasi', 'no_rkm_medis', 'jam_reg', 'kd_pj', 'kd_dokter', 'kd_poli', 'status_poli'],
+    //     order: [
+    //         ['jam_reg', 'DESC'],
+    //     ],
+    // });
+    // for (let element of regBooking) {
 
-        let random2 = getRandomInt(1, 10);
-        let waktuTask2 = convmils(`${element.tgl_registrasi} ${element.jam_reg}`, -random2);
-        let taks2 = {
-            kodebooking: element.no_rawat,
-            taskid: 2,
-            waktu: waktuTask2,
-        };
+    //     let random1 = getRandomInt(10, 15);
+    //     let waktuTask1 = convmils(`${element.tgl_registrasi} ${element.jam_reg}`, -random1);
+    //     let taks1 = {
+    //         kodebooking: element.no_rawat,
+    //         taskid: 1,
+    //         waktu: waktuTask1,
+    //     };
 
-        let waktuTask3 = convmils(`${element.tgl_registrasi} ${element.jam_reg}`, 0);
-        let taks3 = {
-            kodebooking: element.no_rawat,
-            taskid: 3,
-            waktu: waktuTask3,
-        };
+    //     let random2 = getRandomInt(1, 10);
+    //     let waktuTask2 = convmils(`${element.tgl_registrasi} ${element.jam_reg}`, -random2);
+    //     let taks2 = {
+    //         kodebooking: element.no_rawat,
+    //         taskid: 2,
+    //         waktu: waktuTask2,
+    //     };
 
-        let tambah = await addAntrean(data);
-        console.log(tambah);
-        return;
-        if (tambah.metadata.code == 200) {
-            let taksID1 = await updatewaktu(taks1);
-            console.log(taks1);
-            console.log(taksID1);
-            let taksID2 = await updatewaktu(taks2);
-            console.log(taks2);
-            console.log(taksID2);
-            let taksID3 = await updatewaktu(taks3);
-            console.log(taks3);
-            console.log(taksID3);
-        }
-    }
+    //     let waktuTask3 = convmils(`${element.tgl_registrasi} ${element.jam_reg}`, 0);
+    //     let taks3 = {
+    //         kodebooking: element.no_rawat,
+    //         taskid: 3,
+    //         waktu: waktuTask3,
+    //     };
+    //         let taksID1 = await updatewaktu(taks1);
+    //         console.log(taks1);
+    //         console.log(taksID1);
+    //         let taksID2 = await updatewaktu(taks2);
+    //         console.log(taks2);
+    //         console.log(taksID2);
+    //         let taksID3 = await updatewaktu(taks3);
+    //         console.log(taks3);
+    //         console.log(taksID3);
+
+    // }
     // console.log(regBooking.length);
     // console.log(sepNull);
 
 
 }
+// addAntreanJKN("2024-08-14");
 async function addNewAntreanJKN(date) {
     console.log(days(date));
     let res = await getAntrian(date);
@@ -203,9 +132,6 @@ async function addNewAntreanJKN(date) {
         let jeniskunjungan = 3;
         let noRef = `I/${element.no_rawat}`
         let rujukan = await getRujukan(element.pasien.no_peserta);
-        // console.log(element.maping_poli_bpjs.kd_poli_bpjs);
-        // console.log(rujukan.response);
-        // console.log(element.pasien.no_peserta);
         if (rujukan.response == null) {
             let rencanaKontrol = await getlistrencanakontrol(bulan, tahun, element.pasien.no_peserta);
             // console.log(rencanaKontrol.response);
@@ -231,11 +157,6 @@ async function addNewAntreanJKN(date) {
                 noRef = rujukan.response.rujukan[0].noKunjungan;
             } else {
                 jeniskunjungan = 2;
-                // let bulan = element.tgl_registrasi.substring(5, 7)
-                // let tahun = element.tgl_registrasi.substring(0, 4)
-                // let rencanaKontrol = await getlistrencanakontrol(bulan, tahun, element.pasien.no_peserta);
-                // console.log(rencanaKontrol);
-                // return;
             }
         } else {
             jeniskunjungan = 2;
@@ -305,15 +226,15 @@ async function addNewAntreanJKN(date) {
         let tambah = await addAntrean(data);
         console.log(tambah);
         if (tambah.metadata.code == 201) {
-            if (tambah.metadata.message.includes("Rujukan nomor")) {
+            if (tambah.metadata.message.includes("Rujukan")) {
                 data.jeniskunjungan = 2;
                 data.noRef = `I/${element.no_rawat}`
                 console.log(data);
                 tambah = await addAntrean(data);
                 console.log(tambah);
 
+                return;
             }
-            // return;
         }
         if (tambah.metadata.code == 200) {
             let taksID1 = await updatewaktu(taks1);
@@ -659,10 +580,10 @@ cron.schedule('* 7-13 * * 1-6', () => {
 async function backdate(date) {
     await addNewAntreanJKN(date);
     console.log("JKN");
-    // await lajutAja4(date);
-    // console.log("lajutAja4");
-    // await lajutAja5backdate(date);
+    await lajutAja4(date);
+    console.log("lajutAja4");
+    await lajutAja5backdate(date);
     // console.log("lajutAja5backdate");
 }
-backdate("2024-08-05");
+// backdate("2024-08-14");
 
