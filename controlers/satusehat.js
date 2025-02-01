f73665cfbad88c62a17dcb59056b4c3961f6a8d3
fakhryhizballah <falehry88@gmail.com>
2025-02-01 14:36:43 +0700

    kirim oktboer

--- controlers/satusehat.js
+++ controlers/satusehat.js
@@ -14,17 +14,22 @@ async function kirm(date) {
     await updateEncouterRalan(date);
     console.log('done');
 }
+for (let i = 1; i <= 30; i++) {
+    await kirimIGD(`2025-10-${i < 10 ? '0' + i : i}`);
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    await kirm(`2025-10-${i < 10 ? '0' + i : i}`);
+}
 
-// kirm('2025-01-07');
+// kirm('2025-01-08');
 
 async function kirimIGD(date) {
-    // await postEncouterIGD(date);
-    // await pObservation(date);
-    // await pCondition(date);
-    // await pProcedure(date);
-    // await pServiceRequestRadiologi(date);
-    // await pClinicalImpression(date);
+    await postEncouterIGD(date);
+    await pObservation(date);
+    await pCondition(date);
+    await pProcedure(date);
+    await pServiceRequestRadiologi(date);
+    await pClinicalImpression(date);
     await ObservationNyeriIGD(date);
     console.log('done');
 }
-kirimIGD('2024-12-01');
+// kirimIGD('2024-12-02');
