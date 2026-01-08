f3443af20de0841f8b1e420acbb9f6c73080b4bc
fakhryhizballah <falehry88@gmail.com>
2026-01-08 22:23:46 +0700

    hendel error

--- hooks/satusehat.js
+++ hooks/satusehat.js
@@ -72,7 +72,7 @@ async function fetchSatusehat(method, patch, data) {
         console.log(error);
         return {
             total: 0,
-            response: error.response
+            response: error.response.data
         };
     }
 }
