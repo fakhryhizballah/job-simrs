bdf492018086d241ccb0eb8246d611e86c8b4ea1
fakhryhizballah <falehry88@gmail.com>
2026-01-08 22:24:02 +0700

    post enconter test

--- crawling/identitas.js
+++ crawling/identitas.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
 const Practitioner = require("../modelsMongoose/Practitioner");
+const Patient = require("../modelsMongoose/Patient");
+const Encounter = require("../modelsMongoose/Encounter");
 const { satu_sehat_encounter, satu_sehat_mapping_lokasi_ralan, satu_sehat_mapping_lokasi_ranap, resume_pasien_ranap, bangsal, poliklinik, reg_periksa, kamar_inap, kamar, pasien, pegawai, referensi_mobilejkn_bpjs_taskid, diagnosa_pasien, penyakit } = require("../models");
 const { Op } = require("sequelize");
-const { postEncouter, postEncouter2, postData, getIHS, postCondition, getEncounter, getStatus, updateEncounter } = require("../hooks/satusehat");
 const { getlisttask, post } = require("../hooks/bpjs");
 const { fetchSatusehat } = require("../hooks/satusehat");
+const { where } = require('../modelsMongoose/Condition');
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Terhubung ke MongoDB!'))
@@ -22,45 +24,214 @@ mongoose.connection.on('disconnected', () => {
     console.log('Mongoose disconnected from DB');
 });
 
-async function postPractitioner(nik) {
+async function getPractitioner(nik, attributes) {
     let isexist = await Practitioner.findOne({
         'identifier.value': nik
-    })
+    }, attributes)
     if (isexist) {
+        return isexist
+    } else {
+        let cariIHSnumber = await fetchSatusehat("GET", `/Practitioner?identifier=https://fhir.kemkes.go.id/id/nik|${nik}`)
+        console.log(nik)
+        if (cariIHSnumber.total > 0) {
+            console.log(cariIHSnumber.entry[0].resource.id);
+            let dataIHSnumber = await fetchSatusehat("GET", `/Practitioner/${cariIHSnumber.entry[0].resource.id}`)
+            let findPegawai = await pegawai.findOne({
+                attributes: ['no_ktp', 'nama', 'nik'],
+                where: {
+                    no_ktp: nik
+                },
+            })
+            dataIHSnumber.identifier.push({
+                "system": "https://fhir.kemkes.go.id/id/rsid",
+                "value": findPegawai.nik
+            })
+            await Practitioner.create(dataIHSnumber);
+            return dataIHSnumber
+        }
         return false
     }
-    let cariIHSnumber = await fetchSatusehat("GET",`/Practitioner?identifier=https://fhir.kemkes.go.id/id/nik|${nik}`)
+}
+async function getPatient(nik, attributes) {
+    let isexist = await Patient.findOne({
+        'identifier.value': nik
+    }, attributes)
+    if (isexist) {
+        return isexist
+    }
+    let cariIHSnumber = await fetchSatusehat("GET", `/Patient?identifier=https://fhir.kemkes.go.id/id/nik|${nik}`)
     console.log(nik)
     if (cariIHSnumber.total > 0) {
-        console.log(cariIHSnumber.entry[0].resource.id);
-        let dataIHSnumber = await fetchSatusehat("GET", `/Practitioner/${cariIHSnumber.entry[0].resource.id}`)
-        console.log(dataIHSnumber);
+        let findPatient = await pasien.findOne({
+            attributes: ['nm_pasien', 'no_ktp'],
+            where: {
+                no_ktp: nik
+            },
+        })
+        // console.log(findPatient);
+        let dataIHSnumber = cariIHSnumber.entry[0].resource
+        // 1. Cari indeks tempat NIK berada
+        let nikIndex = dataIHSnumber.identifier.findIndex(id =>
+            id.system === "https://fhir.kemkes.go.id/id/nik"
+        );
+        if (nikIndex !== -1) {
+            dataIHSnumber.identifier[nikIndex].value = nik;
+        }
+        if (!findPatient) {
+            await Patient.create(dataIHSnumber);
+            return dataIHSnumber
+        }
+
+        dataIHSnumber.name[0].text = findPatient.nm_pasien
+        await Patient.create(dataIHSnumber);
         return dataIHSnumber
+
+    }
+    return false
+}
+async function getEncounter(subject, identifier, attributes) {
+    let isexist = await Encounter.findOne({
+        'identifier.value': identifier,
+        'subject.reference': `Patient/${subject}`,
+    }, attributes)
+    if (isexist) {
+        return isexist
+    }
+    let cariEncounter = await fetchSatusehat("GET", `/Encounter?subject=${subject}&identifier=${identifier}`)
+    if (cariEncounter.total > 0) {
+        let dataEncounter = await Encounter.create(cariEncounter.entry[0].resource);
+        console.log(dataEncounter)
+        return dataEncounter
     }
     return false
 }
 
-async function petugas() {
-    let findPegawai = await pegawai.findAll({
-        attributes: ['no_ktp', 'nama', 'nik'],
+
+async function postEncouter(date) {
+    let dataReg = await reg_periksa.findAll({
         where: {
-            no_ktp: { [Op.ne]: 0 }
+            tgl_registrasi: date,
+            status_lanjut: 'Ralan',
+            stts: {
+                [Op.not]: 'batal'
+            }
         },
-        // limit: 1
-    })
-    for (let x of findPegawai){
-        let dataIHSnumber = await postPractitioner(x.no_ktp)
-        if (dataIHSnumber) {
-            dataIHSnumber.identifier.push({
-                "system": "https://fhir.kemkes.go.id/id/rsid",
-                "value": x.nik
-            })
-            await Practitioner.create(dataIHSnumber);
+        attributes: ['no_rawat', 'no_rkm_medis', 'kd_dokter', 'kd_poli', 'status_lanjut', 'tgl_registrasi', 'jam_reg'],
+        include: [{
+            model: pasien,
+            as: 'pasien',
+            attributes: ['no_ktp', 'nm_pasien']
+        },
+        {
+            model: pegawai,
+            as: 'pegawai',
+            attributes: ['nama', 'no_ktp'],
+        }, {
+            model: satu_sehat_mapping_lokasi_ralan,
+            as: 'satu_sehat_mapping_lokasi_ralan',
+            attributes: ['id_organisasi_satusehat', 'id_lokasi_satusehat'],
+            // required: false,
+        }, {
+            model: poliklinik,
+            as: 'poliklinik',
+            attributes: ['kd_poli', 'nm_poli']
         }
-        
+        ],
+    })
+    for (let x of dataReg) {
+        let ihsPasen = await getPatient(x.pasien.no_ktp)
+
+        let dataEncounter = await getEncounter(ihsPasen.id, x.no_rawat)
+        if (dataEncounter) {
+            console.log(dataEncounter)
+            console.log("encounter sudah ada")
+            continue
+        }
+        let ihsPetugas = await getPractitioner(x.pegawai.no_ktp, 'id name')
+        console.log(ihsPetugas);
+        let datetime = new Date(x.dataValues.tgl_registrasi + "T" + x.dataValues.jam_reg + ".000Z").toISOString();
+        let newEncounter = {
+            "resourceType": "Encounter",
+            "status": "arrived",
+            "class": {
+                "system": "http://terminology.hl7.org/CodeSystem/v3-ActCode",
+                "code": "AMB",
+                "display": "ambulatory"
+            },
+            "subject": {
+                "reference": "Patient/" + ihsPasen.id,
+                "display": ihsPasen.name[0].text
+            },
+            "participant": [
+                {
+                    "type": [
+                        {
+                            "coding": [
+                                {
+                                    "system": "http://terminology.hl7.org/CodeSystem/v3-ParticipationType",
+                                    "code": "ATND",
+                                    "display": "attender"
+                                }
+                            ]
+                        }
+                    ],
+                    "individual": {
+                        "reference": "Practitioner/" + ihsPetugas.id,
+                        "display": ihsPetugas.name[0].text
+                    }
+                }
+            ],
+            "period": {
+                "start": datetime,
+                "end": datetime
+            },
+            "location": [
+                {
+                    "location": {
+                        "reference": "Location/" + x.dataValues.satu_sehat_mapping_lokasi_ralan.id_lokasi_satusehat,
+                        "display": x.dataValues.poliklinik.nm_poli
+                    }
+                }
+            ],
+            "statusHistory": [
+                {
+                    "status": "arrived",
+                    "period": {
+                        "start": datetime,
+                        "end": datetime
+                    }
+                }
+            ],
+            "serviceProvider": {
+                "reference": "Organization/" + process.env.Organization_id_SATUSEHAT
+            },
+            "identifier": [
+                {
+                    "system": "http://sys-ids.kemkes.go.id/encounter/" + process.env.Organization_id_SATUSEHAT,
+                    "value": x.dataValues.no_rawat
+                }
+            ]
+        }
+        if (x.dataValues.kd_poli == 'IGDK') {
+            newEncounter.class.code = "EMER"
+            newEncounter.class.display = "emergency"
+        }
+        let kirimEncounter = await fetchSatusehat("POST", 'Encounter', newEncounter);
+        console.log(newEncounter);
+        if (kirimEncounter.error) {
+            console.log(kirimEncounter.error);
+            continue;
+        }
+        await Encounter.create(kirimEncounter);
+        console.log(ihsPasen.id, x.no_rawat);
     }
-    mongoose.disconnect();
-    console.log('selesai')
+
 }
-petugas();
+postEncouter('2026-01-08')
+
+
+module.exports = {
+    getPractitioner,
+    getPatient
+}
 
--- modelsMongoose/Encounter.js
+++ modelsMongoose/Encounter.js
@@ -1,69 +1,13 @@
 const mongoose = require('mongoose');
 
 const encounterSchema = new mongoose.Schema({
-    class: {
-        code: { type: String, required: true },
-        display: { type: String, required: true },
-        system: { type: String, required: true }
+    id: {
+        type: String,
+        unique: true
     },
-    id: { type: String, required: true, unique: true },
-    identifier: [
-        {
-            system: { type: String, required: true },
-            value: { type: String, required: true }
-        }
-    ],
-    location: [
-        {
-            location: {
-                display: { type: String, required: true },
-                reference: { type: String, required: true }
-            }
-        }
-    ],
-    meta: {
-        lastUpdated: { type: Date, required: true },
-        versionId: { type: String, required: true }
-    },
-    participant: [
-        {
-            individual: {
-                display: { type: String, required: true },
-                reference: { type: String, required: true }
-            },
-            type: [
-                {
-                    coding: [
-                        {
-                            code: { type: String, required: true },
-                            display: { type: String, required: true },
-                            system: { type: String, required: true }
-                        }
-                    ]
-                }
-            ]
-        }
-    ],
-    period: {
-        start: { type: Date, required: true }
-    },
-    resourceType: { type: String, required: true },
-    serviceProvider: {
-        reference: { type: String, required: true }
-    },
-    status: { type: String, required: true },
-    statusHistory: [
-        {
-            period: {
-                start: { type: Date, required: true }
-            },
-            status: { type: String, required: true }
-        }
-    ],
-    subject: {
-        display: { type: String, required: true },
-        reference: { type: String, required: true }
+    resourceType: {
+        type: String
     }
-}, { timestamps: true });
+}, { strict: false, versionKey: false });
 
 module.exports = mongoose.model('Encounter', encounterSchema);
