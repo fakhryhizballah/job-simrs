61c006397f63a85f1b2667a91b0d6caa04291ab6
fakhryhizballah <falehry88@gmail.com>
2026-05-23 16:01:16 +0700

    update

--- crawling/sendnow.js
+++ crawling/sendnow.js
@@ -11,7 +11,8 @@ async function kirm(date) {
     await pCondition(date)
     await new Promise(resolve => setTimeout(resolve, 2000));
     await pProcedure(date)
-
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    await updateEncounter(date)    
     console.log('done');
 }
-kirm('2025-01-01');
+kirm('2026-01-01');
