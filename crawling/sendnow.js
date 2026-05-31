a75cf5754d123d71dc36b56ff4b099f65a3ce2f2
fakhryhizballah <falehry88@gmail.com>
2026-05-31 15:50:45 +0700

    update kirimInstuksiDiet

--- crawling/sendnow.js
+++ crawling/sendnow.js
@@ -2,19 +2,22 @@ const { postEncouter, updateEncounter, updateEncounterRanap } = require("./ident
 const { pCondition, pProcedure } = require("./icd.js");
 const { kirimMedicationRequest, kirimMedicationDispense } = require("./Medication.js");
 const { kirimObservation } = require("./Observation.js");
+const { kirimInstuksiDiet } = require("./Composition.js");
 async function kirm(date) {
-    await postEncouter(date)
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    await kirimObservation(date)
-    await pCondition(date)
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    await pProcedure(date)
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    await updateEncounter(date)
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    await updateEncounterRanap(date)
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    await kirimMedicationRequest(date)
+    // await postEncouter(date)
+    // await new Promise(resolve => setTimeout(resolve, 2000));
+    // await kirimObservation(date)
+    // await pCondition(date)
+    // await new Promise(resolve => setTimeout(resolve, 2000));
+    await kirimInstuksiDiet(date)
+    // await new Promise(resolve => setTimeout(resolve, 2000));
+    // await pProcedure(date)
+    // await new Promise(resolve => setTimeout(resolve, 2000));
+    // await updateEncounter(date)
+    // await new Promise(resolve => setTimeout(resolve, 2000));
+    // await updateEncounterRanap(date)
+    // await new Promise(resolve => setTimeout(resolve, 2000));
+    // await kirimMedicationRequest(date)
     // await new Promise(resolve => setTimeout(resolve, 2000));
     // await kirimMedicationDispense(date)
     console.log('done' + date);
